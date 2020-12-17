e30d78beaea122dc1d263f3ded687210314b576e
Murat Kaan Ilhan <mkmcmxci@gmail.com>
2020-12-17 22:29:49 +0300

    Added some solutions

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] list = {5,2,1,3,4};
 
-        System.out.println(Implementation.permutationEquation(list));
 
     }
 
